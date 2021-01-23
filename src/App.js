2ad8c7c3f679ccfd593b26a7126553bfb72870f1
dmc09 <dharminlive1213@gmail.com>
2021-01-22 21:01:58 -0600

    Able to sucessfully hit endpoint with Cors Anywhere proxy and env variable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import { useState } from "react";
+import axios from 'axios'
 
 const {REACT_APP_KEY} = process.env
+console.log()
 
 function App() {
-  const api = "";
+const test =`https://cors-anywhere.herokuapp.com/https://api.sportradar.us/nba/trial/v5/en/games/2013/12/21/schedule.json?api_key=${REACT_APP_KEY}`
+  const api =  "https://api.sportradar.us/nba/trial/v5/en/games/2013/12/21/schedule.json?api_key=${REACT_APP_KEY}";
+
+
   const [data, setData] = ('');
 console.log(REACT_APP_KEY,'Dis is the key');
+axios.get(test).then(res=>console.log(res))
   return (
     <div className="App">
       <p>hello</p>

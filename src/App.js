73dd460d60e1e51fee9e9d2deeab4f1ae4893b4e
dmc09 <dharminlive1213@gmail.com>
2021-01-31 20:11:04 -0600

    Successfully able to capture all necessary pieces of state.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,66 @@
-
 // IMPORT STATEMENTS
 import "./App.css";
-import React, { useState,useContext, useEffect } from "react";
-import { CategoryContext} from './context/categoryContext'
+import React, { useState, useContext, useEffect } from "react";
+import { CategoryContext } from "./context/categoryContext";
 import axios from "axios";
 
 // MAIN VARIABLE DECLARTIONS
 const { REACT_APP_KEY } = process.env;
 const api = `https://cors-anywhere.herokuapp.com/https://api.sportradar.us/nba/trial/v7/en/seasons/2020/REG/leaders.json?api_key=${REACT_APP_KEY}`;
+
 // COMPONENT
-const App = () =>  {
-const [data,setData] = useContext(CategoryContext) //Object is not iterable (cannot read
+const App = () => {
+  const [
+    data, setData,
+     minutes, setMinutes,
+     points,setPoints,
+     rebounds,setRebounds,
+     assists,setAssists,
+   steals,setSteals,
+ blocks,setBlocks,
+threes,setThrees,
+misc,setMisc] = useContext(CategoryContext);
 
 
-// Meant to run only once after initial render
-useEffect(() => {
-  getData();
-}, []);
+  // Meant to run only once after initial render
 
-async function getData() {
-  try {
-    const response = await axios.get(api);
-    await setData(response.data.categories);
-  } catch (error) {
-    console.error(error);
+  useEffect(() => {
+    const filterForPoints = data && data.filter((set) => set.name === "points");
+    const filterForMinutes = data && data.filter(set=> set.name === 'minutes');
+    const filterForRebounds = data && data.filter(set=> set.name === 'rebounds');
+    const filterForAssists = data && data.filter(set=> set.name === 'assists');
+    const filterForSteals = data && data.filter(set=> set.name === 'steals');
+    const filterForBlocks = data && data.filter(set=> set.name === 'blocks');
+    const filterForMadeThress = data && data.filter(set=> set.name === 'three_points_made');
+    const filterForThreePCT = data && data.filter(set=> set.name === 'three_points_pct');
+    const filterForTurnovers = data && data.filter(set=> set.name === 'turnovers');
+    const filterForPersoFouls = data && data.filter(set=> set.name === 'personal_fouls');
+    const filterForTechFouls = data && data.filter(set=> set.name === 'tech_fouls');
+    const filterForFoulOuts = data && data.filter(set=> set.name === 'foulouts');
+    getData();
+    setMinutes(filterForMinutes);
+    setPoints(filterForPoints);
+    setRebounds(filterForRebounds);
+    setAssists(filterForAssists);
+    setSteals(filterForSteals);
+    setBlocks(filterForBlocks)
+    setThrees([...filterForMadeThress,...filterForThreePCT]);
+    setMisc([...filterForTurnovers,...filterForPersoFouls,...filterForTechFouls,...filterForFoulOuts])
+
+  }, [data]);
+
+  async function getData() {
+    try {
+      const response = await axios.get(api);
+      await setData(response.data.categories);
+    } catch (error) {
+      console.error(error);
+    }
   }
-}
-  return (
-      <div className="App">
-        <p>hello</p>
-        <h2>cheeseCake!</h2>
-      </div>
-  );
-}
+
+  return <div className="App">
+<p>test</p>
+  </div>;
+};
 
 export default App;
-
-
-
-
-
-
-
-
-
-// const filterForMinutes = mainData.filter(set=> set.name === 'minutes');
-// const filterForPoints = mainData.filter(set=> set.name === 'points');
-// const filterForRebounds = mainData.filter(set=> set.name === 'rebounds');
-// const filterForAssists = mainData.filter(set=> set.name === 'assists');
-// const filterForSteals = mainData.filter(set=> set.name === 'steals');
-// const filterForBlocks = mainData.filter(set=> set.name === 'blocks');
-// const filterForMadeThress = mainData.filter(set=> set.name === 'three_points_made');
-// const filterForThreePCT = mainData.filter(set=> set.name === 'three_points_pct');
-// const filterForTurnovers = mainData.filter(set=> set.name === 'turnovers');
-// const filterForPersoFouls = mainData.filter(set=> set.name === 'personal_fouls');
--- src/context/categoryContext.js
+++ src/context/categoryContext.js
@@ -3,10 +3,31 @@ export const CategoryContext = createContext();
 
 
 export const CategoryProvider = (props) =>{
-const [data,setData] = useState();
+
+const [data,setData] = useState([]);
+const [minutes,setMinutes] = useState([]);
+const [points,setPoints] = useState([]);
+const [rebounds,setRebounds] = useState([]);
+const [assists,setAssists] = useState([]);
+const [steals,setSteals] = useState([]);
+const [blocks,setBlocks] = useState([]);
+const [threes,setThrees] = useState([]);
+const [misc,setMisc] = useState([]);
+// const [currentStat,setCurrentState] = useState(minutes);
+// const [orderBy,setOrderBy] = useState(total);
   return (
     <CategoryContext.Provider
-      value={[data,setData]}>
+      value={[
+      data,setData,
+      minutes,setMinutes,
+      points,setPoints,
+      rebounds,setRebounds,
+      assists,setAssists,
+      steals,setSteals,
+      blocks,setBlocks,
+      threes,setThrees,
+      misc,setMisc
+      ]}>
       {props.children}
     </CategoryContext.Provider>
   );

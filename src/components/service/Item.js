c873c6c2f1e1c3c6205f4b6a38f0bb5f978fa020
“Dharmin <dharminlive1213@mail.com>
2021-02-27 19:46:31 -0600

    updated with basic styling.

--- src/components/service/Item.js
+++ src/components/service/Item.js
@@ -54,13 +54,21 @@ function Item({ info, index }) {
 
   return (
     <div className="item-container">
+      <div className="headshot-container">
       <img className='headshot'
         src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${pID}.png`}
         />
+      </div>
       <li className="item-info">
-        {info.tied === "false" ? info.rank : index + 1}. {info.player.full_name}=== {info.score < 1 ? `${(info.score*100).toFixed(1)}%`   :info.score} and he plays for the {info.teams[0].market}{" "}
-        {info.teams[0].name} ({info.player.jersey_number}{" "}
-        {info.player.primary_position} ){" "}
+        <span className="item-rank">{info.tied === "false" ? info.rank : index + 1}</span>
+
+       {info.player.full_name} {info.score < 1 ? `${(info.score*100).toFixed(1)}%`   :info.score}  {info.teams[0].market}{" "}
+        {info.teams[0].name}
+<div className="item-jersey_position">
+        {info.player.jersey_number}
+        <hr />
+        {info.player.primary_position}
+        </div>
       </li>
     </div>
   );

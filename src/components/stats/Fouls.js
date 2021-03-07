44b799dead1398a5486afee0f9e9626b7449ef9f
“Dharmin <Dharminlive1213@gmail.com>
2021-03-06 19:24:43 -0600

    Fouls Component working.

--- src/components/stats/Fouls.js
+++ src/components/stats/Fouls.js
@@ -1,12 +1,27 @@
-import React,{useEffect,useState} from 'react';
+import React,{useEffect,useState,useContext} from 'react';
+import { CategoryContext } from "../../context/categoryContext";
 import Item from '../service/Item';
 import {v4 as uuidv4 } from 'uuid';
 
 function Fouls({totalFouls,avgFouls}) {
-
+  const[data, setData,
+   minutes, setMinutes,
+   points,setPoints,
+   rebounds,setRebounds,
+   assists,setAssists,
+   steals,setSteals,
+    blocks,setBlocks,
+  threes,setThrees,
+  fouls,setFouls,
+  turnovers,setTurnovers,
+  techfoulouts,setTechFoulOuts,
+  currentStat,setCurrentStat,
+  headshotData,setheadshotData,
+  orderBy,setOrderBy,
+  ] = useContext(CategoryContext)
   let viewingStat = totalFouls;
-
-
+  if(orderBy === 'total') { viewingStat= totalFouls};
+  if(orderBy === 'average') { viewingStat= avgFouls};
   let TopTenList= viewingStat.ranks.slice(0,10);
   let list = TopTenList.map((item,index)=> <Item key={uuidv4()} index={index} info={item}/>)
 

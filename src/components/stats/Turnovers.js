7390ee0902e1bbcb69419b32a336442bc436f500
“Dharmin <Dharminlive1213@gmail.com>
2021-03-06 20:09:30 -0600

    Turnovers component working.

--- src/components/stats/Turnovers.js
+++ src/components/stats/Turnovers.js
@@ -1,13 +1,29 @@
-import React,{useEffect,useState} from 'react';
+import React,{useEffect,useState,useContext} from 'react';
+import { CategoryContext } from "../../context/categoryContext";
 import Item from '../service/Item';
 import {v4 as uuidv4 } from 'uuid';
 
 function Turnovers({totalTurnovers,avgTurnovers}) {
-
+  const[data, setData,
+   minutes, setMinutes,
+   points,setPoints,
+   rebounds,setRebounds,
+   assists,setAssists,
+   steals,setSteals,
+    blocks,setBlocks,
+  threes,setThrees,
+  fouls,setFouls,
+  turnovers,setTurnovers,
+  techfoulouts,setTechFoulOuts,
+  currentStat,setCurrentStat,
+  headshotData,setheadshotData,
+  orderBy,setOrderBy,
+  ] = useContext(CategoryContext)
 
   let viewingStat = totalTurnovers;
 
-
+  if(orderBy === 'total') { viewingStat= totalTurnovers};
+  if(orderBy === 'average') { viewingStat= avgTurnovers};
   let TopTenList= viewingStat.ranks.slice(0,10);
   let list = TopTenList.map((item,index)=> <Item key={uuidv4()} index={index} info={item}/>)
 

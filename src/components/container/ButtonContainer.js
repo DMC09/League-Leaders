f77a1173bfa68b65622b682616b6edcde76b0b7d
“Dharmin <Dharminlive1213@gmail.com>
2021-04-14 21:10:15 -0500

    moved button container to its own component.

---
+++ src/components/container/ButtonContainer.js
@@ -0,0 +1,33 @@
+import React,{useEffect, useContext,useState} from 'react';
+import { CategoryContext } from "../../context/categoryContext";
+
+
+
+function ButtonContainer(){
+  const [data, setData,
+    headshotData,setheadshotData,
+   minutes, setMinutes,
+   points,setPoints,
+   rebounds,setRebounds,
+   assists,setAssists,
+ steals,setSteals,
+blocks,setBlocks,
+threes,setThrees,
+fouls,setFouls,
+turnovers,setTurnovers,
+techfoulouts,setTechFoulOuts,
+currentStat,setCurrentStat,
+orderBy,setOrderBy] = useContext(CategoryContext);
+  return <div className="button-container">
+    <button
+      disabled={orderBy==="total"}
+      onClick={()=>setOrderBy('total')}>Total</button>
+      <button
+        disabled={orderBy==="average"}
+        onClick={()=>setOrderBy('average')}>Averages</button>
+  {currentStat == 'threes' &&  <button disabled={orderBy==="percentage"} onClick={()=>setOrderBy('percentage')}>Percentages</button>}
+    </div>
+}
+
+
+export default ButtonContainer
--- src/components/service/StatChanger.js
+++ src/components/service/StatChanger.js
@@ -1,6 +1,7 @@
 import React,{useEffect,useContext,useState} from "react";
 import { CategoryContext } from "../../context/categoryContext";
 import Logo from '../../Logo.png';
+import ButtonContainer from '../container/ButtonContainer';
 
 
 function StatChanger() {
@@ -79,15 +80,7 @@ useEffect(() => {
 
     {currentStat === null  &&  <div className="button-container"></div>}
 
-      {currentStat !== null  &&  <div className="button-container">
-        <button
-          disabled={orderBy==="total"}
-          onClick={()=>setOrderBy('total')}>Total</button>
-          <button
-            disabled={orderBy==="average"}
-            onClick={()=>setOrderBy('average')}>Averages</button>
-      {currentStat == 'threes' &&  <button disabled={orderBy==="percentage"} onClick={()=>setOrderBy('percentage')}>Percentages</button>}
-        </div> }
+      {currentStat !== null  && <ButtonContainer/> }
   </div>
   ;
 }

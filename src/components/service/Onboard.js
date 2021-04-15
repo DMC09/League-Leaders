ed9207be8e53c678827231ad8f8fa34218e19f6b
“Dharmin <Dharminlive1213@gmail.com>
2021-04-15 17:48:32 -0500

    changed onboarding text.

--- src/components/service/Onboard.js
+++ src/components/service/Onboard.js
@@ -5,12 +5,12 @@ function Onboard()  {
   return <div className="Onboard-container">
         <h1>
             <Fade top>
-              Welcome to the League Leaders Web App!
+              Welcome to the League Leaders Web App
             </Fade>
           </h1>
         <p>
             <Fade bottom>
-              Select a stat see who are the who is leading in their respective stat
+              Select a stat see who leading the league!
             </Fade>
           </p>
 
